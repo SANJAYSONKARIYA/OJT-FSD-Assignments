0a2377a14f9bacab8d88f949ec1818bce177dbcc
SANJAY SONKARIYA <119073413+SANJAYSONKARIYA@users.noreply.github.com>
2024-06-13 15:31:49 +0530

    Delete Assignments/28-05-2024 directory

--- Assignments/28-05-2024/Circle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Circle {
-    public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        System.out.print("Enter the radius of the circle:");
-        int radius = a.nextInt();
-        System.out.println("Area of circle = " + Math.PI*radius*radius);
-    }    
-}
